896d160a3c35582d63be93baf98e1d5a1fb7adf7
Никита Титов <ntitovgo@yandex.ru>
2025-09-24 15:12:32 +0300

    add: добавил роуты Home и ChatPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
+import { Route } from "react-router-dom";
 import { io } from "socket.io-client";
+import { Home } from "./components/home/home";
+import { ChatPage } from "./components/chat/chat";
 const socket = io("http://localhost:5001");
 
 function App() {
   return (
-    <div className="App">
-      <h1>Websocket</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/chat" element={<ChatPage />} />
+    </Routes>
   );
 }
 
