865c66113a77e4a807adb9dfe115f9319e9553d1
Никита Титов <ntitovgo@yandex.ru>
2025-09-24 16:48:51 +0300

    add: стили для чата

--- client/src/components/chat/chat.jsx
+++ client/src/components/chat/chat.jsx
@@ -1,12 +1,13 @@
 import { Body } from "./components/body/body"
 import { MessageBlock } from "./components/message-block/message-block"
 import { Sidebar } from "./components/sidebar/sidebar"
+import styles from "./styles.module.css"
 
 export const ChatPage = ({socket}) => {
   return (
-    <div className="chat">
+    <div className={styles.chat}>
         <Sidebar />
-        <main className="main">
+        <main className={styles.main}>
             <Body />
             <MessageBlock />
         </main>
