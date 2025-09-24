e198c72190aed549d5e66175cb53e30c24c95698
Никита Титов <ntitovgo@yandex.ru>
2025-09-24 17:08:15 +0300

    add: добавил стили в sidebar

--- client/src/components/chat/components/sidebar/sidebar.jsx
+++ client/src/components/chat/components/sidebar/sidebar.jsx
@@ -2,9 +2,9 @@ import styles from "./styles.module.css"
 
 export const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <h4 className="header">Users</h4>
-      <ul className="users">
+    <div className={styles.sidebar}>
+      <h4 className={styles.header}>Users</h4>
+      <ul className={styles.users}>
         <li>User 1</li>
         <li>User 2</li>
         <li>User 3</li>
