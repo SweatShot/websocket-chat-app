17292e209c7b52aafe71de7453b92e9ae696fb3a
Никита Титов <ntitovgo@yandex.ru>
2025-09-24 17:13:08 +0300

    add: стили для message

--- client/src/components/chat/components/message-block/message-block.jsx
+++ client/src/components/chat/components/message-block/message-block.jsx
@@ -2,10 +2,10 @@ import styles from "./styles.module.css"
 
 export const MessageBlock = () => {
   return (
-    <div className="message-block">
-      <form>
-        <input type="text" className="user-message" />
-        <button>Сказать</button>
+    <div className={styles.messageBlock}>
+      <form className={styles.form}>
+        <input type="text" className={styles.userMessage} />
+        <button className={styles.btn}>Сказать</button>
       </form>
     </div>
   );
